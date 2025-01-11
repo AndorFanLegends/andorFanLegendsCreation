f26365bda703e4315a756b121ce4ac13f4d084a4
dchansel <damien@13ruedebrioiux.com>
2025-01-11 13:52:29 +0100

    add some style and text in howto

--- src/components/HowToView/HowToView.vue
+++ src/components/HowToView/HowToView.vue
@@ -31,85 +31,93 @@
                 <v-btn icon @click="closeDialog"> <v-icon>mdi-close</v-icon> </v-btn>
             </v-toolbar>
             <v-card-text>
-                <h2>Available Markdowns</h2>
-                <v-table>
-                    <thead>
-                    <tr>
-                        <th class="text-left">Use</th>
-                        <th class="text-left">Markdown</th>
-                        <th class="text-left">Rendered Output</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Bold : To bold text, add two asterisks or underscores before and after a word or phrase.</td>
-                            <td>Put a **Gor** in case **39**</td>
-                            <td v-html="marked('Put a **Gor** in case **39**')"></td>
-                        </tr>
-                        <tr>
-                            <td>Italic : .</td>
-                            <td>*Once upon a time, in andor*</td>
-                            <td v-html="marked('*Once upon a time, in andor*')"></td>
-                        </tr>
-                        <tr>
-                            <td>Separator</td>
-                            <td>***</td>
-                            <td v-html="marked('***')"></td>
-                        </tr>
-                        <tr>
-                            <td>Blockquote</td>
-                            <td>></td>
-                            <td v-html="marked('>')"></td>
-                        </tr>
-                        <tr>
-                            <td>Dices</td>
-                            <td>:D1: :D2: :D3: :D4: :D5: </td>
-                            <td v-html="marked(':D1: :D2: :D3: :D4: :D5:')"></td>
-                        </tr>
-                        <tr>
-                            <td>Black Dices</td>
-                            <td>:DN6: :DN8: :DN10: :DN12:</td>
-                            <td v-html="marked(':DN6: :DN8: :DN10: :DN12:')"></td>
-                        </tr>
-                        <tr>
-                            <td>Players</td>
-                            <td>:2P: / :3P: / :4P: </td>
-                            <td v-html="marked(':2P: / :3P: / :4P:')"></td>
-                        </tr>
-                    </tbody>
-                </v-table>
-                <v-spacer><br></v-spacer>
-                <h2>Some composite examples</h2>
+                <v-row justify="center"> 
+                    <v-col cols="12" md="6">
 
-                <h3>Dice choices</h3>
-                <br>
-                > :D1: :D2: :D3: **falcon**<br>
-                Get the falcon<br>
-                <br>
-                <div v-html="marked('> :D1: :D2: :D3: **falcon**\
-                / <br>Get the falcon')"></div>
+                        <h2>Available Markdowns</h2>
+                        <v-table>
+                            <thead>
+                            <tr>
+                                <th class="text-left font-weight-bold">Use</th>
+                                <th class="text-left font-weight-bold">Markdown</th>
+                                <th class="text-left font-weight-bold">Rendered Output</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Bold : To bold text, add two asterisks or underscores before and after a word or phrase.</td>
+                                    <td>Put a **Gor** in case **39**</td>
+                                    <td v-html="marked('Put a **Gor** in case **39**')"></td>
+                                </tr>
+                                <tr>
+                                    <td>Italic : .</td>
+                                    <td>*Once upon a time, in andor*</td>
+                                    <td v-html="marked('*Once upon a time, in andor*')"></td>
+                                </tr>
+                                <tr>
+                                    <td>Separator</td>
+                                    <td>***</td>
+                                    <td v-html="marked('***')"></td>
+                                </tr>
+                                <tr>
+                                    <td>Blockquote</td>
+                                    <td>></td>
+                                    <td v-html="marked('>')"></td>
+                                </tr>
+                                <tr>
+                                    <td>Dices</td>
+                                    <td>:D1: :D2: :D3: :D4: :D5: :D6:</td>
+                                    <td v-html="marked(':D1: :D2: :D3: :D4: :D5: :D6:')"></td>
+                                </tr>
+                                <tr>
+                                    <td>Black Dices</td>
+                                    <td>:DN6: :DN8: :DN10: :DN12:</td>
+                                    <td v-html="marked(':DN6: :DN8: :DN10: :DN12:')"></td>
+                                </tr>
+                                <tr>
+                                    <td>Players</td>
+                                    <td>:2P: / :3P: / :4P: </td>
+                                    <td v-html="marked(':2P: / :3P: / :4P:')"></td>
+                                </tr>
+                            </tbody>
+                        </v-table>
+                        <v-spacer><br></v-spacer>
+                        <h2>Some composite examples</h2>
+                        <h3>Dice choices</h3>
+                        <br>
+                        > :D1: :D2: :D3: **falcon**<br>
+                        Get the falcon<br>
+                        <br>
+                        <div v-html="marked('> :D1: :D2: :D3: **falcon**\
+                        / <br>Get the falcon')"></div>
+                        <v-spacer><br></v-spacer>
+                        <h3>Missions</h3>
 
-                <h3>Missions</h3>
-                <br>
-                ***
-                ** Mission :**
-                ***
-                <br>
-                <v-spacer><br></v-spacer>
-                <h2>How add your legend to the Legend App</h2>
-                <p>
-                    <b>For new legend</b>, create new issue in <a href="https://github.com/dchansel/andorFanLegends">dchansel/andorFanLegends</a> with [New Legend] Your Legend Title<br>  
-                    Link : <a href="https://github.com/dchansel/andorFanLegends/issues/new?title=[New Legend]">New Legend</a><br>
-                    Don't forget to add the legend <b>Language</b> in description, and other necessary information.<br>
-                    Then attach the Json File to the issue, and submit.
-                </p>
-                <v-spacer><br></v-spacer>
-                <p>
-                    <b>For update legend</b>, create new issue in <a href="https://github.com/dchansel/andorFanLegends">dchansel/andorFanLegends</a> with [Update Legend] Your Legend Title<br>
-                    Link : <a href="https://github.com/dchansel/andorFanLegends/issues/new?title=[Update Legend]">Update Legend</a><br>
-                    Don't forget to add the legend <b>Language</b> in description, and other necessary information.<br>
-                    Then attach the Json File to the issue, and submit.
-                </p>
+                        ***<br>
+                        ** Mission :**<br>
+                        Text for mission<br>
+                        ***<br>
+                        <br>
+                        <div v-html="marked('***\n**Mission :**\nText for mission\n***')"></div>
+                        <v-spacer><br></v-spacer>
+                        <h2>How request for adding your legend to the Legends App (<a href="https://andorfanlegends.web.app/">link</a>)</h2>
+                        After create or update a Legend, then save it in json format.<br>
+                        So,
+                        <p>
+                            <b>For new legend</b>, create new issue in <a href="https://github.com/dchansel/andorFanLegends">dchansel/andorFanLegends</a> with [New Legend] Your Legend Title<br>  
+                            Link : <a href="https://github.com/dchansel/andorFanLegends/issues/new?title=[New Legend]">New Legend</a><br>
+                            Don't forget to add the legend <b>Language</b> in description, and other necessary information.<br>
+                            Then attach the Json File to the issue, and submit.
+                        </p>
+                        <v-spacer><br></v-spacer>
+                        <p>
+                            <b>For update legend</b>, create new issue in <a href="https://github.com/dchansel/andorFanLegends">dchansel/andorFanLegends</a> with [Update Legend] Your Legend Title<br>
+                            Link : <a href="https://github.com/dchansel/andorFanLegends/issues/new?title=[Update Legend]">Update Legend</a><br>
+                            Don't forget to add the legend <b>Language</b> in description, and other necessary information.<br>
+                            Then attach the Json File to the issue, and submit.
+                        </p>
+                    </v-col>
+                </v-row>
             </v-card-text>
         </v-card>
     </v-dialog>
